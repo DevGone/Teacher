89352f067da0588704bb48848a48167a4e3655ca
Tanguy Hélesbeux <tanguy.helesbeux@gmail.com>
2015-03-31 18:54:31 +0200

    improved data in overview

--- app/overview/overview.js
+++ app/overview/overview.js
@@ -12,62 +12,72 @@ angular.module('teacher.overview', ['ngRoute'])
 .controller('OverviewCtrl', [function() {
 
   Morris.Area({
-      element: 'morris-area-chart',
+      element: 'class-points-history-chart',
       data: [{
-          period: '2010 Q1',
-          iphone: 2666,
-          ipad: null,
-          itouch: 2647
+          period: '2015 W1',
+          group1: 2666,
+          group2: null,
+          group3: 2647,
+          group4: 1000
       }, {
-          period: '2010 Q2',
-          iphone: 2778,
-          ipad: 2294,
-          itouch: 2441
+          period: '2015 W2',
+          group1: 2778,
+          group2: 2294,
+          group3: 2441,
+          group4: 1000
       }, {
-          period: '2010 Q3',
-          iphone: 4912,
-          ipad: 1969,
-          itouch: 2501
+          period: '2015 W3',
+          group1: 4912,
+          group2: 1969,
+          group3: 2501,
+          group4: 1000
       }, {
-          period: '2010 Q4',
-          iphone: 3767,
-          ipad: 3597,
-          itouch: 5689
+          period: '2015 W4',
+          group1: 3767,
+          group2: 3597,
+          group3: 5689,
+          group4: 1000
       }, {
-          period: '2011 Q1',
-          iphone: 6810,
-          ipad: 1914,
-          itouch: 2293
+          period: '2015 W5',
+          group1: 6810,
+          group2: 1914,
+          group3: 2293,
+          group4: 1000
       }, {
-          period: '2011 Q2',
-          iphone: 5670,
-          ipad: 4293,
-          itouch: 1881
+          period: '2015 W6',
+          group1: 5670,
+          group2: 4293,
+          group3: 1881,
+          group4: 1000
       }, {
-          period: '2011 Q3',
-          iphone: 4820,
-          ipad: 3795,
-          itouch: 1588
+          period: '2015 W7',
+          group1: 4820,
+          group2: 3795,
+          group3: 1588,
+          group4: 1000
       }, {
-          period: '2011 Q4',
-          iphone: 15073,
-          ipad: 5967,
-          itouch: 5175
+          period: '2015 W8',
+          group1: 15073,
+          group2: 5967,
+          group3: 5175,
+          group4: 1000
       }, {
-          period: '2012 Q1',
-          iphone: 10687,
-          ipad: 4460,
-          itouch: 2028
+          period: '2015 W9',
+          group1: 10687,
+          group2: 4460,
+          group3: 2028,
+          group4: 1000
       }, {
-          period: '2012 Q2',
-          iphone: 8432,
-          ipad: 5713,
-          itouch: 1791
+          period: '2015 W10',
+          group1: 8432,
+          group2: 5713,
+          group3: 1791,
+          group4: 1000
       }],
       xkey: 'period',
-      ykeys: ['iphone', 'ipad', 'itouch'],
-      labels: ['iPhone', 'iPad', 'iPod Touch'],
-      pointSize: 2,
+      ykeys: ['group1', 'group2', 'group3', 'group4'],
+      labels: ['Groupe n°1', 'Groupe n°2', 'Groupe n°3', 'Groupe n°4'],
+      pointSize: 3,
       hideHover: 'auto',
       resize: true,
       lineColors: ['#2E79B9', '#58B957', '#F2AE43', '#DB524B']
