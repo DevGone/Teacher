7c91158960d8454e7ef1faa69e9c2c94d1c6204e
amallecourt <adrien.mallecourt@gmail.com>
2015-04-05 23:55:56 -0400

    add some content to the teacher setting screen

--- app/settings/settings.js
+++ app/settings/settings.js
@@ -9,6 +9,39 @@ angular.module('teacher.settings', ['ngRoute'])
   });
 }])
 
-.controller('SettingsCtrl', [function() {
+.controller('SettingsCtrl', ['$scope', function($scope) {
+    var nbGroup = 4;
 
+    $( "ul.droptrue" ).sortable({
+      connectWith: "ul"
+    });
+ 
+    $( "#sortable1" ).disableSelection();
+
+    $( "#dialogStudent" ).dialog({
+      autoOpen: false,
+      width: 800,
+      height: 300, 
+      buttons: {
+        "Ajouter": function() {
+          $( "#dialogStudent" ).dialog( "close" );
+        },
+        "Annuler": function() {
+          $( "#dialogStudent" ).dialog( "close" );
+        }
+      }
+    });
+
+    $scope.addStudentEvent = function() {
+        $( "#dialogStudent" ).dialog( "open" );
+    };
+
+    $scope.addGroupEvent = function() {
+        nbGroup = nbGroup +1;
+        var groupHtml = '<div class="col-lg-6"><div class="panel panel-info"><div class="panel-heading">' +
+            'Groupe ' + nbGroup + '</div><div class="panel-body"><ul id="sortable5" class="droptrue">' + 
+            '</ul></div></div></div>';
+        var newGroupDiv = document.getElementById("newGroupDiv");
+        newGroupDiv.innerHTML = newGroupDiv.innerHTML + groupHtml;
+    };
 }]);
