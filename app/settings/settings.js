d5a1102c11cd1ae23880b676bf318f9ce182a24a
amallecourt <adrien.mallecourt@gmail.com>
2015-04-06 22:26:39 -0400

    no more dialog, good screen for demo

--- app/settings/settings.js
+++ app/settings/settings.js
@@ -10,38 +10,55 @@ angular.module('teacher.settings', ['ngRoute'])
 }])
 
 .controller('SettingsCtrl', ['$scope', function($scope) {
-    var nbGroup = 4;
+    var nbGroup = 3;
 
-    $( "ul.droptrue" ).sortable({
-      connectWith: "ul"
-    });
- 
-    $( "#sortable1" ).disableSelection();
+    $( "ul.sortableUL" ).sortable({
+      connectWith: "ul",
+      dropOnEmpty: true
+    }).disableSelection();
 
-    $( "#dialogStudent" ).dialog({
-      autoOpen: false,
-      width: 800,
-      height: 300, 
-      buttons: {
-        "Ajouter": function() {
-          $( "#dialogStudent" ).dialog( "close" );
-        },
-        "Annuler": function() {
-          $( "#dialogStudent" ).dialog( "close" );
-        }
-      }
-    });
+    // $( "#dialogStudent" ).dialog({
+    //   autoOpen: false,
+    //   width: 800,
+    //   height: 300, 
+    //   buttons: {
+    //     "Ajouter": function() {
+    //       $( "#dialogStudent" ).dialog( "close" );
+    //     },
+    //     "Annuler": function() {
+    //       $( "#dialogStudent" ).dialog( "close" );
+    //     }
+    //   }
+    // });
 
-    $scope.addStudentEvent = function() {
-        $( "#dialogStudent" ).dialog( "open" );
-    };
+    // $scope.addStudentEvent = function() {
+    //     $( "#dialogStudent" ).dialog( "open" );
+    // };
+
+      $scope.optionsGroup = [
+        { label: 'Groupe 1', value: 1 },
+        { label: 'Groupe 2', value: 2 },
+        { label: 'Groupe 3', value: 3 }
+    ];
 
     $scope.addGroupEvent = function() {
         nbGroup = nbGroup +1;
-        var groupHtml = '<div class="col-lg-6"><div class="panel panel-info"><div class="panel-heading">' +
-            'Groupe ' + nbGroup + '</div><div class="panel-body"><ul id="sortable5" class="droptrue">' + 
+        var groupHtml = '<div class="col-lg-6"><div class="panel panel-red"><div class="panel-heading">' +
+            'Groupe ' + nbGroup + '</div><div class="panel-body"><ul id="ulGroup4" class="sortableUL">' + 
             '</ul></div></div></div>';
         var newGroupDiv = document.getElementById("newGroupDiv");
         newGroupDiv.innerHTML = newGroupDiv.innerHTML + groupHtml;
+        $scope.optionsGroup.push({ label: 'Groupe 4', value: 4 })
+        $("ul.sortableUL").sortable({connectWith: "ul", dropOnEmpty: true}).disableSelection();
     };
+
+     $scope.addStudent = function(user) {
+       var groupId = user.group.value;
+       var groupUl = document.getElementById("ulGroup" + groupId);
+       var newline = '<li class="ui-state-default">' + user.name + '</li>';
+       groupUl.innerHTML = groupUl.innerHTML + newline;
+
+     }
+
+
 }]);
