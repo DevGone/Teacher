1f037cd68c9f5427079d7ade80a942465d4f5a29
Tanguy Hélesbeux <tanguy.helesbeux@gmail.com>
2015-04-07 12:39:09 +0200

    added socket logic to mascot view

--- app/mascot/mascot.js
+++ app/mascot/mascot.js
@@ -9,6 +9,41 @@ angular.module('teacher.mascot', ['ngRoute'])
   });
 }])
 
-.controller('MascotCtrl', [function() {
+.controller('MascotCtrl', ["$scope", function ($scope) {
+
+  var socket = io('http://devgone.herokuapp.com');
+
+  socket.emit('identification', { device: 'teacher'});
+
+// For debug only
+  // var walter = io('http://devgone.herokuapp.com');
+  // walter.emit('identification', { device: 'walter'});
+  // walter.on('setColor', function(data) {
+  //   console.log('Color set to: ' + data);
+  // });
+
+  var yellowColor = {
+    red: 255,
+    green: 246,
+    blue: 26
+  };
+
+  var challengerColor = {
+    red: 70,
+    green: 70,
+    blue: 255
+  };
+
+  var setColor = function (color) {
+    socket.emit('setColor', color);
+  };
+
+  $scope.sendYellowEvent = function() {
+    setColor(yellowColor);
+  };
+
+  $scope.resetColor = function () {
+    setColor(challengerColor);
+  };
 
 }]);
