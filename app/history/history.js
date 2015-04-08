b48b4b7fb90c368aba3bd3a147b5a0bc5b45be1f
Tanguy Hélesbeux <tanguy.helesbeux@gmail.com>
2015-04-08 10:02:24 +0200

    more accurate stats

--- app/history/history.js
+++ app/history/history.js
@@ -15,64 +15,64 @@ angular.module('teacher.history', ['ngRoute'])
       element: 'class-points-history-chart',
       data: [{
           period: '2015 W1',
-          group1: 2666,
-          group2: null,
-          group3: 2647,
-          group4: 1000
+          group1: 137,
+          group2: 158,
+          group3: 129,
+          group4: 132
       }, {
           period: '2015 W2',
-          group1: 2778,
-          group2: 2294,
-          group3: 2441,
-          group4: 1000
+          group1: 145,
+          group2: 137,
+          group3: 167,
+          group4: 112
       }, {
           period: '2015 W3',
-          group1: 4912,
-          group2: 1969,
-          group3: 2501,
-          group4: 1000
+          group1: 213,
+          group2: 345,
+          group3: 237,
+          group4: 222
       }, {
           period: '2015 W4',
-          group1: 3767,
-          group2: 3597,
-          group3: 5689,
-          group4: 1000
+          group1: 432,
+          group2: 498,
+          group3: 387,
+          group4: 407
       }, {
           period: '2015 W5',
-          group1: 6810,
-          group2: 1914,
-          group3: 2293,
-          group4: 1000
+          group1: 456,
+          group2: 476,
+          group3: 489,
+          group4: 424
       }, {
           period: '2015 W6',
-          group1: 5670,
-          group2: 4293,
-          group3: 1881,
-          group4: 1000
+          group1: 645,
+          group2: 434,
+          group3: 587,
+          group4: 556
       }, {
           period: '2015 W7',
-          group1: 4820,
-          group2: 3795,
-          group3: 1588,
-          group4: 1000
+          group1: 703,
+          group2: 712,
+          group3: 687,
+          group4: 693
       }, {
           period: '2015 W8',
-          group1: 15073,
-          group2: 5967,
-          group3: 5175,
-          group4: 1000
+          group1: 478,
+          group2: 734,
+          group3: 701,
+          group4: 675
       }, {
           period: '2015 W9',
-          group1: 10687,
-          group2: 4460,
-          group3: 2028,
-          group4: 1000
+          group1: 723,
+          group2: 691,
+          group3: 704,
+          group4: 698
       }, {
           period: '2015 W10',
-          group1: 8432,
-          group2: 5713,
-          group3: 1791,
-          group4: 1000
+          group1: 730,
+          group2: 717,
+          group3: 678,
+          group4: 657
       }],
       xkey: 'period',
       ykeys: ['group1', 'group2', 'group3', 'group4'],
